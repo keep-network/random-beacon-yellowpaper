e904af621747efc626c784d82712e49433321541
Łukasz Zimnoch <lukasz.zimnoch@akena.co>
2019-09-13 17:03:21 +0200

    Comment rewording

--- dkg/dkg.py
+++ dkg/dkg.py
@@ -323,8 +323,7 @@ broadcast(messagePhase10(disqualifiedKeys))
 #     IA if no message sent
 #
 # Validate:
-# - all expected private keys of disqualified members
-#     which have sent valid shares in phase 4 are revealed
+# - all private keys of members disqualified after phase 5
 #     DQ if number of keys is incorrect
 # - each revealed private key must be a valid scalar for ECDH
 #     DQ if a private key is invalid
